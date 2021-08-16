0ff68c7fe0ac5bd5d0720bdf95d4be8adf6b5cf7
Craig Harvey <crharvey0@gmail.com>
2021-08-16 20:59:33 +1000

    Copy script from original source

--- networking/listening_on.sh
+++ networking/listening_on.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
+#######################################################################################
 # Simple script to show which app is listening on a particular port. 
 #
-# Usage: listening_on.sh <port>
+# Returns the process that is listening on the specified port
+# Currently only tested on ubuntu 14.04
+#
+# Usage: listening_on <port_number>
 #
 
+PORT_NUM=$(netstat -tulpn 2>&1 | grep $1 | grep tcp | awk 'match($7, "/") {print substr($7, 1, RSTART-1)}')
 
+ps -ef | grep $PORT_NUM | grep -v grep
