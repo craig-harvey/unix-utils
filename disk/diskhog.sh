9b9bf45492008918f2a28085586459a6ddf91af5
Craig Harvey <craigh@vrt.com.au>
2022-01-13 19:35:13 +1000

    Added help message

--- disk/diskhog.sh
+++ disk/diskhog.sh
@@ -9,11 +9,13 @@
 # Options:
 #   -d<dir>  Start from directory <dir> instead of the current directory
 #   -n<num>  Only list the top <num> entries
+#   -h       Display the help message and exit
 #
 
 
 DIR='.'
 NUM='-0'
+HELP=0
 
 for ARGUMENT in "$@"
 do
@@ -24,11 +26,21 @@ do
     case "$PARAM" in
             d)              DIR=$VALUE ;;
             n)		    NUM=$VALUE ;;
+            h)		    HELP=1 ;;
             *)   
     esac    
 
-
 done
 
+if [ $HELP -eq 1 ]; then
+    echo "diskhog"
+    echo "Displays the size of each subdirectory. Sorted from largest to smallest."
+    echo "Parameters:"
+    echo "  -d<dir>   Start from directory <dir> instead of the current directory"
+    echo "  -n<num>   Only list the top <num> entries (Default: List all subdirectories)"
+    exit 0
+fi
+
+echo "Compiling data ..."
 du -h -d 1 $DIR 2>/dev/null | sort -t \t -hrk1 | head -n$NUM
 
